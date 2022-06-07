94200626d814cd45649eceff9c324ee1af3a4618
Renos Bardis <renosbardis@gmail.com>
2022-06-07 12:13:22 +0200

    Feature: Adding comments on controller layer

--- src/main/java/com/example/imdb/controller/ActorsController.java
+++ src/main/java/com/example/imdb/controller/ActorsController.java
@@ -13,6 +13,12 @@ public class ActorsController {
 
     private final ActorService actorService;
 
+    /**
+     * This method returns a TitleBasicDTO object of two actors and the list of movies that both are played.
+     * @param actor1 The first actor.
+     * @param actor2 The second actor.
+     * @return A TitleBasicDTO object.
+     */
     @GetMapping("/actor1/{actor1}/actor2/{actor2}/movies")
     public TitleBasicDTO getMoviesAndTvShowsByActors(@PathVariable(value="actor1") String actor1, @PathVariable(value="actor2") String actor2) {
         return actorService.getMoviesByActors(actor1, actor2);
